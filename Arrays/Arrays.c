ac451d2851e0884adb2fb912057eff885e7ec6d0
Sagar Chopra <schopra@firemate.com>
2022-07-02 17:53:19 +0530

    Merging two sorted arrays

--- Arrays/Arrays.c
+++ Arrays/Arrays.c
@@ -337,9 +337,38 @@ void MoveNegativesOnLeftSide(struct Array* arr)
     } 
 }
 
+struct Array* Merge(struct Array* arr1, struct Array* arr2)
+{
+    int i, j, k;
+    i = j = k = 0;
+
+    struct Array* arr3 = (struct Array*)malloc(sizeof(struct
+        Array));
+
+    while (i < arr1->length && j < arr2->length)
+    {
+        if (arr1->A[i] < arr2->A[j])
+            arr3->A[k++] = arr1->A[i++];
+        else
+            arr3->A[k++] = arr2->A[j++];
+    }
+    for (; i < arr1->length; i++)
+        arr3->A[k++] = arr1->A[i];
+    for (; j < arr2->length; j++)
+        arr3->A[k++] = arr2->A[j];
+    arr3->length = arr1->length + arr2->length;
+    arr3->size = 10;
+
+    return arr3;
+}
+
 int main() {
     struct Array arr = { {2,3,4,5,6},10,5 };
     //struct Array arr = { {2,-3,25,10,-15,-7},10,5 };
+    struct Array arr1 = { {2,6,10,15,25},10,5 };
+    struct Array arr2 = { {3,4,7,18,20},10,5 };
+    struct Array* arr3;
+
     Display(arr);
     printf("\n");
     //Append(&arr,7);
@@ -365,8 +394,10 @@ int main() {
     //RightRotation(&arr);
     //InsertInSortedArray(&arr,3);
     //printf("%d\n", IsSorted(arr));
-    MoveNegativesOnLeftSide(&arr);  
+    //MoveNegativesOnLeftSide(&arr);
+    arr3 = Merge(&arr1, &arr2);
+    Display(*arr3);
     printf("\n");
-    Display(arr);
+    //Display(arr);
     return 0;
 }
