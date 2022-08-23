d7754fa9d00508ecfbc73b724c429ba260956cbb
Sagar Chopra <schopra@firemate.com>
2022-08-23 18:35:42 +0530

    Count,Add, Max, Search with Recursive versions

--- LinkedList/LinkedList.cpp
+++ LinkedList/LinkedList.cpp
@@ -23,6 +23,15 @@ public:
 	~LinkedList();
 	void Display();
 	void RecursiveDisplay(Node *t);
+	int Count();
+	int RecursiveCount(Node* t);
+	int Add();
+	int RecursiveAdd(Node* t);
+	int Max();
+	int RecursiveMax(Node* t);
+	Node* Search(int key);
+	Node* RecursiveSearch(Node* t, int key);
+	Node* Search_MoveToHead(int key);
 };
 
 LinkedList::LinkedList(int A[], int n)
@@ -82,13 +91,179 @@ void LinkedList::RecursiveDisplay(Node *t)
 }
 
 
+int LinkedList::Count()
+{
+	Node* p = first;
+
+	int count = 0;
+	while (p)
+	{
+		count++;
+		p = p->next;
+	}
+
+	return count;
+}
+
+
+int LinkedList::RecursiveCount(Node *t)
+{
+	Node* p = t;
+	
+	if (p == 0)
+	{
+		return 0;				
+	}
+	return RecursiveCount(p->next) + 1;	
+}
+
+int LinkedList::Add()
+{
+	Node* p = first;
+
+	int sum = 0;
+	while (p)
+	{		
+		sum += p->data;
+		p = p->next;
+	}
+
+	return sum;
+}
+
+int LinkedList::RecursiveAdd(Node* t)
+{
+	Node* p = t;
+
+	if (p == 0)
+	{
+		return 0;
+	}
+	return RecursiveAdd(p->next) + p->data;
+}
+
+int LinkedList::Max()
+{
+	Node* p = first;
+	int max = INT16_MIN;
+
+	while (p)
+	{
+		if (p->data > max)
+		{
+			max = p->data;
+		}		
+		p = p->next;
+	}
+
+	return max;
+}
+
+int LinkedList::RecursiveMax(Node* t)
+{
+	Node* p = t;
+	int max = INT16_MIN;
+	int x = 0;
+
+	if (p == 0)
+	{
+		return max;
+	}
+
+	x = RecursiveMax(p->next);
+	return x > p->data ? x : p->data;			
+}
+
+Node* LinkedList::Search(int key)
+{
+	Node* p = first;
+
+	while (p)
+	{
+		if (p->data == key)
+		{
+			return p;
+		}
+		p = p->next;
+	}
+	return NULL;
+}
+
+Node* LinkedList::RecursiveSearch(Node* t,int key)
+{
+	Node* p = t;
+
+	while (p)
+	{
+		if (p == NULL)
+			return NULL;
+		if (key == p->data)
+			return p;
+		return RecursiveSearch(p->next, key);
+	}
+}
+
+Node* LinkedList::Search_MoveToHead(int key)
+{
+	Node* p = first;
+	Node* q = NULL;
+
+	while (p)
+	{		
+		
+		if (p->data == key)
+		{
+			q->next = p->next;
+			p->next = first;
+			first = p;
+			return p;
+		}
+		q = p;
+		p = p->next;
+	}
+	return NULL;
+}
+
 int main()
 {
 	int A[]{ 3,5,7,10,15 };
 	
-	LinkedList list(A, 5);
+	LinkedList list(A,5);
 	//list.Display();
+	//list.RecursiveDisplay(list.first);
+	//cout << list.Count();
+	//cout << list.RecursiveCount(list.first);
+	//cout << list.Add();
+	//cout << list.RecursiveAdd(list.first);
+	//cout << list.Max();
+	//cout << list.RecursiveMax(list.first);
 
-	list.RecursiveDisplay(list.first);
+	/*if (list.Search(7))
+	{
+		cout << "Key Found";
+	}
+	else
+	{
+		cout << "Key Not Found";
+	}*/
 
+	//if (list.RecursiveSearch(list.first,7))
+	//{
+	//	cout << "Key Found";
+	//}
+	//else
+	//{
+	//	cout << "Key Not Found";
+	//}
+
+	if (list.Search_MoveToHead(7))
+	{
+		cout << "Key Found"<<endl;
+	}
+	else
+	{
+		cout << "Key Not Found"<<endl;
+	}
+
+	list.Display();
 }
