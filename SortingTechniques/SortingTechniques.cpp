9e52979c65926200d428a896f65db67dd87cea50
Sagar Chopra <schopra@firemate.com>
2022-10-09 14:53:02 +0530

    Shell Sort

--- SortingTechniques/SortingTechniques.cpp
+++ SortingTechniques/SortingTechniques.cpp
@@ -371,6 +371,20 @@ void RadixSort(int A[], int n) {
 	delete[] bins;
 }
 
+// Code is similar to Insertion Sort with some modifications
+void ShellSort(int A[], int n) {
+	for (int gap = n / 2; gap >= 1; gap /= 2) {
+		for (int j = gap; j < n; j++) {
+			int temp = A[j];
+			int i = j - gap;
+			while (i >= 0 && A[i] > temp) {
+				A[i + gap] = A[i];
+				i = i - gap;
+			}
+			A[i + gap] = temp;
+		}
+	}
+}
 
 void Print(int A[], int n)
 {
