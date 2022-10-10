6b3ce1e46ad75e20f8b00e82313f9f53a89b7692
Sagar Chopra <schopra@firemate.com>
2022-10-10 16:52:28 +0530

    Linear Probing

--- Hashing/Chaining.cpp
+++ Hashing/Chaining.cpp
@@ -86,21 +86,21 @@ HashTable::~HashTable() {
 	delete[] HT;
 }
 
-int main() {
-	int A[] = { 16, 12, 25, 39, 6, 122, 5, 68, 75 };
-	int n = sizeof(A) / sizeof(A[0]);
-	HashTable H;
-	for (int i = 0; i < n; i++) {
-		H.Insert(A[i]);
-	}
-	cout << "Successful Search" << endl;
-	int key = 6;
-	int value = H.Search(key);
-	cout << "Key: " << key << ", Value: " << value << endl;
-	cout << "Unsuccessful Search" << endl;
-	key = 95;
-	value = H.Search(key);
-	cout << "Key: " << key << ", Value: " << value << endl;
-
-	return 0;
-}
+//int main() {
+//	int A[] = { 16, 12, 25, 39, 6, 122, 5, 68, 75 };
+//	int n = sizeof(A) / sizeof(A[0]);
+//	HashTable H;
+//	for (int i = 0; i < n; i++) {
+//		H.Insert(A[i]);
+//	}
+//	cout << "Successful Search" << endl;
+//	int key = 6;
+//	int value = H.Search(key);
+//	cout << "Key: " << key << ", Value: " << value << endl;
+//	cout << "Unsuccessful Search" << endl;
+//	key = 95;
+//	value = H.Search(key);
+//	cout << "Key: " << key << ", Value: " << value << endl;
+//
+//	return 0;
+//}
