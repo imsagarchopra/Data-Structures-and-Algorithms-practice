e5163b0a2b3714bf555cb26af718ad3114abc73d
Sagar Chopra <schopra@firemate.com>
2022-11-03 18:14:04 +0530

    Kruskal MST

--- Graph/PrimMST.cpp
+++ Graph/PrimMST.cpp
@@ -74,22 +74,22 @@ void PrimsMST(int G[V][V], int n) {
 	PrintMST(T, G);
 }
 
-int main() {
-
-	int cost[V][V]{
-			{I, I, I, I, I, I, I, I},
-			{I, I, 25, I, I, I, 5, I},
-			{I, 25, I, 12, I, I, I, 10},
-			{I, I, 12, I, 8, I, I, I},
-			{I, I, I, 8, I, 16, I, 14},
-			{I, I, I, I, 16, I, 20, 18},
-			{I, 5, I, I, I, 20, I, I},
-			{I, I, 10, I, 14, 18, I, I},
-	};
-
-	int n = sizeof(cost[0]) / sizeof(cost[0][0]) - 1;
-
-	PrimsMST(cost, n);
-
-	return 0;
-}
+//int main() {
+//
+//	int cost[V][V]{
+//			{I, I, I, I, I, I, I, I},
+//			{I, I, 25, I, I, I, 5, I},
+//			{I, 25, I, 12, I, I, I, 10},
+//			{I, I, 12, I, 8, I, I, I},
+//			{I, I, I, 8, I, 16, I, 14},
+//			{I, I, I, I, 16, I, 20, 18},
+//			{I, 5, I, I, I, 20, I, I},
+//			{I, I, 10, I, 14, 18, I, I},
+//	};
+//
+//	int n = sizeof(cost[0]) / sizeof(cost[0][0]) - 1;
+//
+//	PrimsMST(cost, n);
+//
+//	return 0;
+//}
